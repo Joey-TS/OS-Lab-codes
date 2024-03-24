bb12e9fda4597469de1bd58e83c3da9129f7ca5d
Joseph T S <josephshyju96@gmail.com>
2024-03-24 21:52:29 +0530

    Added Arrival time feature to fcfs

--- fcfs.c
+++ fcfs.c
@@ -2,9 +2,7 @@
 
 int main()
 {
-    int pid[15];
-    int bt[15];
-    int n;
+    int pid[15], bt[15], n, arr[15], total=0, ct[15], wt[15];
 
     printf("Enter the number of processes: ");
     scanf("%d",&n);
@@ -16,20 +14,87 @@ int main()
     printf("Enter burst time of all the processes: ");
     for(int i=0;i<n;i++)
         {scanf("%d",&bt[i]);}
+
+    printf("Enter arrival time of all the processes: ");
+    for(int i=0;i<n;i++)
+        {scanf("%d",&arr[i]);}
+
     int i;
-    int wt[15];
     wt[0]=0;
+
+    for (i=0; i<n; i++)
+        {
+            for(int j=0; j<n-1; j++)
+                {
+                    if(arr[j]>arr[j+1])
+                        {
+                            int temp=arr[j];
+                            arr[j]=arr[j+1];
+                            arr[j+1]=temp;
+
+                            temp=bt[j];
+                            bt[j]=bt[j+1];
+                            bt[j+1]=temp;
+
+                            temp=pid[j];
+                            pid[j]=pid[j+1];
+                            pid[j+1]=temp;
+                        }
+                }
+        }
+
+    //for calculating completion time of each process
+    ct[0]=arr[0]+bt[0];
+    for(i=1; i<n; i++)
+        {
+            int temp=0;
+            if(arr[i]>ct[i-1])
+                {temp=arr[i]-ct[i-1];}
+            ct[i]=ct[i-1]+bt[i]+temp;
+        }
 
     //for calculating waiting time of each process
     for(i=1; i<n; i++)
-        {wt[i]= bt[i-1]+ wt[i-1];}
+        {wt[i]= ct[i]-arr[i]-bt[i];}
 
-    printf("Process ID     Burst Time     Waiting Time     TurnAround Time\n");
+    //re-sort the processes according to their process id
+    for (i=0; i<n; i++)
+        {
+            for(int j=0; j<n-1; j++)
+                {
+                    if(pid[j]>pid[j+1])
+                        {
+                            int temp=arr[j];
+                            arr[j]=arr[j+1];
+                            arr[j+1]=temp;
+
+                            temp=bt[j];
+                            bt[j]=bt[j+1];
+                            bt[j+1]=temp;
+
+                            temp=pid[j];
+                            pid[j]=pid[j+1];
+                            pid[j+1]=temp;
+
+                            temp=ct[j];
+                            ct[j]=ct[j+1];
+                            ct[j+1]=temp;
+
+                            temp=wt[j];
+                            wt[j]=wt[j+1];
+                            wt[j+1]=temp;
+                        }
+                }
+        }
+
+
+    printf("Process ID     Arrival Time    Burst Time     Waiting Time     TurnAround Time\n");
     float twt=0.0;
     float tat= 0.0;
     for(i=0; i<n; i++)
     {
         printf("%d\t\t", pid[i]);
+        printf("%d\t\t", arr[i]);
         printf("%d\t\t", bt[i]);
         printf("%d\t\t", wt[i]);
         //calculating and printing turnaround time of each process
